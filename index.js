c6a0465d1cd7e900f1ac9a027367ae201b9cf8c0
mdew2222 <122666780+mdew2222@users.noreply.github.com>
2023-01-14 03:35:25 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+public class Circle {
+    private double radius;
+    private static final double PI = 3.14;
+    
+    // Constructor that takes the radius as an input
+    public Circle(double radius) {
+        this.radius = radius;
+    }
